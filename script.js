513426cc7b613711443974d0bac59a0dcb98e2ca
biswajit <bisu.rnj@gmail.com>
2022-10-17 14:37:04 +0530

    lets try

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-try{
-    var a=10;
-    function f(){
-        document.getElementById("letFunction").innerHTML = a;
-    }
-    f();
-    document.getElementById("varFunction").innerHTML = a;
+// try{
+//     var a=10;
+//     function f(){
+//         document.getElementById("letFunction").innerHTML = a;
+//     }
+//     f();
+//     document.getElementById("varFunction").innerHTML = a;
     
-}catch(err){
-    document.getElementById("constFunction").innerHTML = err.message;
+// }catch(err){
+//     document.getElementById("constFunction").innerHTML = err.message;
 
-}
+// }
